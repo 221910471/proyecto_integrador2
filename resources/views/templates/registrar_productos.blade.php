66d4a8f1c8079c4502236532494a9e029056ded7
Cristian Flores <al221910343@gmail.com>
2021-02-23 22:40:33 -0600

    Validar formulario registrar producto

--- resources/views/templates/registrar_productos.blade.php
+++ resources/views/templates/registrar_productos.blade.php
@@ -12,6 +12,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
 
+	<script src=" {{ asset('js/jquery-3.3.1.js') }} "></script>
+	<script src=" {{ asset('js/jquery-ui.js') }} "></script>
 </head>
 
 <body class="is-preload">
@@ -31,34 +33,35 @@
 					{{ csrf_field() }}
 
 					<div style="padding: 1%;">
-						Nombre Del Producto : <input type="text" name="nombre_producto" value="{{ old('nombre_producto')}}">
+						<td>Nombre Del Producto :</td>
+						<td><input type="text" name="nombre_producto" id="nombre_producto" value="{{ old('nombre_producto')}}"></td>
 					</div>
 					@if($errors->first('nombre_producto')) <i>{{$errors -> first ('nombre_producto')}}</i>@endif
 
 					<div style="padding: 1%;">
-						Numero de existencias : <input type="number" name="no_existencias" value="{{ old('no_existencias')}}">
+						Numero de existencias : <input type="text" name="no_existencias" id="no_existencias" value="{{ old('no_existencias')}}">
 					</div>
 					@if($errors->first('no_existencias')) <i>{{$errors -> first ('no_existencias')}}</i>@endif
 					
 
 					<div style="padding: 1%;">
-						Precio : <input type="number" name="precio"><br>
+						Precio : <input type="text" name="precio" id="precio"><br>
 					</div>
 					@if($errors->first('precio')) <i>{{$errors -> first ('precio')}}</i>@endif
 
 					<div style="padding: 1%;">
 					<!-- como cambiar el tamaño -->
-						Descripcion : <input type="textarea" name="descripcion" value="{{ old('descripcion')}}"><br>
+						Descripcion : <input type="text" name="descripcion" id="descripcion" value="{{ old('descripcion')}}"><br>
 					</div>
 					@if($errors->first('descripcion')) <i>{{$errors -> first ('descripcion')}}</i>@endif
 
 					<div style="padding: 1%;">
-						Medida : <input type="number" name="medida"><br>
+						Medida : <input type="text" name="medida" id="medida"><br>
 					</div>
 					@if($errors->first('medida')) <i>{{$errors -> first ('medida')}}</i>@endif
 
 					<div style="padding: 1%;">
-						Precio oferta : <input type="number" name="precio_oferta"><br>
+						Precio oferta : <input type="text" name="precio_oferta" id="precio_oferta"><br>
 					</div>
 					@if($errors->first('precio_oferta')) <i>{{$errors -> first ('precio_oferta')}}</i>@endif
 					
@@ -75,7 +78,78 @@ Grupo : <input type="text" name="grupo"><br>
 
 			</div>
 		</div>
+		<script>
+		$(document).ready(function(){
+			$("#nombre_producto").keyup(function(){
+							var txtapm = $("#nombre_producto").val();
+                            var formato = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+							
+                            if(formato.test(txtapm)){ $("#nombre_producto").css({"border": "1px solid #0F0"}).fadeIn(2000); }
+                            else{ $("#nombre_producto").css({"border": "1px solid #F00"}).fadeIn(2000); }
+                        });
+			// -------------------------------------------------------------
+			const $input = document.querySelector("#no_existencias");
+                            const patron = /[0-9]+/;
 
+                            $input.addEventListener("keydown", event => {
+                            console.log(event.key);
+                            if(patron.test(event.key)){
+                                $('#no_existencias').css({ "border":"1px solid #0C0" });
+                            }  
+                            else{
+                                if(event.keyCode==8){ console.log("backspace"); }
+                                else{ event.preventDefault(); }
+                            }  
+                            });
+				
+			//--------------------------------------------------------------------
+			const $precio = document.querySelector("#precio");
+                            $precio.addEventListener("keydown", event => {
+                            console.log(event.key);
+                            if(patron.test(event.key)){
+                                $('#precio').css({ "border":"1px solid #0C0" });
+                            }  
+                            else{
+                                if(event.keyCode==8){ console.log("backspace"); }
+                                else{ event.preventDefault(); }
+                            }  
+                            });
+			// -------------------------------------------------------------------
+			$("#descripcion").keyup(function(){
+							var txtapm = $("#descripcion").val();
+                            var formato = /^[A-Za-z0-9\_\-\.\s\xF1\xD1]+$/;
+						
+                            if(formato.test(txtapm)){ $("#descripcion").css({"border": "1px solid #0F0"}).fadeIn(2000); }
+                            else{ $("#descripcion").css({"border": "1px solid #F00"}).fadeIn(2000); }
+                        });
+			// -----------------------------------------------------------------------
+			const $medida = document.querySelector("#medida");
+                            $medida.addEventListener("keydown", event => {
+                            console.log(event.key);
+                            if(patron.test(event.key)){
+                                $('#medida').css({ "border":"1px solid #0C0" });
+                            }  
+                            else{
+                                if(event.keyCode==8){ console.log("backspace"); }
+                                else{ event.preventDefault(); }
+                            }  
+                            });
+			// ------------------------------------------------------------------------------
+			const $precio_oferta = document.querySelector("#precio_oferta");
+                            $precio_oferta.addEventListener("keydown", event => {
+                            console.log(event.key);
+                            if(patron.test(event.key)){
+                                $('#precio_oferta').css({ "border":"1px solid #0C0" });
+                            }  
+                            else{
+                                if(event.keyCode==8){ console.log("backspace"); }
+                                else{ event.preventDefault(); }
+                            }  
+                            });
+					});
+		
+		</script>
+		
 		@include ('layouts.menu')
 
 	</div>
